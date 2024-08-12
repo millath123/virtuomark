282eb9a646697ce3ff4919897e21435db0b4556a
Adila millath <138776924+millath123@users.noreply.github.com>
2024-08-12 14:35:55 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,8 @@ import User from '../models/User.js';
 import generateToken from '../utils/token.js';
 import bcrypt from 'bcryptjs';
 import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+dotenv.config();
 
 // Setup Nodemailer transporter
 const transporter = nodemailer.createTransport({
